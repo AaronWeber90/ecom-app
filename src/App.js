bc88ea8248519572a095a668468ec11012db13d2
Aaron Weber <aaweber@prodyna.com>
2022-07-07 23:44:35 +0200

    seperate nav component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route, Outlet } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
 const Shop = () => {
   return <p>SHOP</p>;
 };
 
-const Nav = () => {
-  return (
-    <div>
-      <p>NAVIGATION</p>
-      <Outlet />
-    </div>
-  );
-};
-
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Nav />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="home" element={<Home />} />
       </Route>
     </Routes>
   );
